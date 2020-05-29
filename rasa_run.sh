4a830ec53bba49fea1f1647d4615bca27cefad4e
Michael Fekadu <mike.fekadu@gmail.com>
2020-05-29 08:16:52 -0700

    more verbosity

--- rasa_run.sh
+++ rasa_run.sh
@@ -19,4 +19,5 @@ rasa run -vv \
     --model labs/lab3rasa/models \
     --verbose \
     --endpoints labs/lab3rasa/endpoints.yml \
+    --debug \
     $1
--- rasa_run_actions.sh
+++ rasa_run_actions.sh
@@ -3,4 +3,7 @@
 # This will run as a standalone server.
 # separate from the `rasa shell` or the `rasa run`
 # https://rasa.com/docs/rasa/user-guide/connectors/your-own-website/#rest-channels
-rasa run -vv actions --actions labs.lab3rasa.actions
+rasa run -vv actions \
+    --actions labs.lab3rasa.actions \
+    --debug \
+    $1
