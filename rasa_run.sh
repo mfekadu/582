525e93da51229d34b2b0b9b61cdbaae1819dde34
Michael Fekadu <mike.fekadu@gmail.com>
2020-05-29 08:06:34 -0700

    provide rasa the env var && rename env vars

--- rasa_run.sh
+++ rasa_run.sh
@@ -1,2 +1,22 @@
 # https://rasa.com/docs/rasa/user-guide/connectors/your-own-website/#rest-channels
-rasa run -vv --model labs/lab3rasa/models --verbose --endpoints labs/lab3rasa/endpoints.yml
+
+
+# https://stackoverflow.com/a/2013589
+export RASA_ACTION_ENDPOINT=${RASA_ACTION_ENDPOINT:="http://localhost:5055/webhook"}
+
+echo "RASA_ACTION_ENDPOINT is " $RASA_ACTION_ENDPOINT
+
+## given the environment variable, run rasa
+#RASA_ACTION_ENDPOINT=$RASA_ACTION_ENDPOINT \
+#    rasa run -vv \
+#    --model labs/lab3rasa/models \
+#    --verbose \
+#    --endpoints labs/lab3rasa/endpoints.yml \
+#    $1
+
+
+rasa run -vv \
+    --model labs/lab3rasa/models \
+    --verbose \
+    --endpoints labs/lab3rasa/endpoints.yml \
+    $1
