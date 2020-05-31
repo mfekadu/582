a58422a1355871414a7598ede0c8f84f360c03e5
Michael Fekadu <mike.fekadu@gmail.com>
2020-05-30 22:47:37 -0700

    needs action endpoint to run

--- rasa_shell.sh
+++ rasa_shell.sh
@@ -1,3 +1,7 @@
+export RASA_ACTION_ENDPOINT=${RASA_ACTION_ENDPOINT:="http://localhost:5055/webhook"}
+
+echo "RASA_ACTION_ENDPOINT is " $RASA_ACTION_ENDPOINT
+
 rasa shell -vv \
     --model labs/lab3rasa/models \
     --verbose \
